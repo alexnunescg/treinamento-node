d5d9c2cbc6bacfdd7f05bd90509eff1321870b6e
Alexandre Nunes <alexnunescg@gmail.com>
2019-10-26 00:18:12 -0300

    Remocao de todo o conteudo

--- routers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    app.get('/', (req, res) => {
-        res.status(200).json({
-            status: 'Api OK. Server is listening !!!'
-        });
-    });
-}
--- routers/students_basic.js
+++
@@ -1,9 +0,0 @@
-// module.exports = app => {
-//     const StudentController = app.controllers.student;
-    
-//     app.post('/students', StudentController.create);
-//     app.get('/students', StudentController.findAll);
-//     app.get('/students/:id', StudentController.findById);
-//     app.put('/students/:id', StudentController.update);
-//     app.delete('/students/:id', StudentController.delete);
-// }
